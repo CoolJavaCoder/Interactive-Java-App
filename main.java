4103e52a86293a042e5de6a6a97b783e5656404a
BeCharlie32 <BeCharlie32@outlook.com>
2025-08-01 07:53:58 +1000

    Set up home page

--- main.java
+++ main.java
@@ -1,19 +1,30 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class main {
     public static void main(String[] args){
         Window window = new Window();
 
         JLabel titlepg1 = new JLabel();
-        titlepg1.setBounds(163,100,80,50);
+        titlepg1.setBounds(115,0,200,50);
+        titlepg1.setFont(new Font("Arial", Font.BOLD, 20));
+        titlepg1.setText("Welcome to JDocs!");
 
         JButton startbtn = new JButton();
-        startbtn.setBounds(163,100,80,50);
+        startbtn.setBounds(105,75,210,50);
         startbtn.setFont(new Font("Arial", Font.BOLD, 20));
-        startbtn.setText("Start");
+        startbtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                startbtn.setVisible(false);
+                titlepg1.setVisible(false);
+            }
+        });
+        startbtn.setText("Start JDocs Editor");
 
-
+        window.add(titlepg1);
         window.add(startbtn);
     }
 }
